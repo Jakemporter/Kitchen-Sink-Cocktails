52c68f12677fa80b7888114f95119a07225ed94f
Jake Porter <Jake.m.porter@gmail.com>
2022-07-26 14:32:01 -0500

    added hidden api key and made search able to use multiple ingredients

--- script.js
+++ script.js
@@ -1,76 +1,73 @@
 // search by ingredient fetch
 
 // to do: .push? to create a list of the drinks in display not just the last one
+var apiKey = keys.API_KEY;
 
 var searchInput;
 
 function handleSearchFormSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
+  var ingredientInputVal = $("#ingredient-input").val();
+  var nameInputVal = $("#cocktail-search").val();
 
-    var ingredientInputVal = $('#ingredient-input').val()
-    var nameInputVal = $('#cocktail-search').val()
-    
+  if (!ingredientInputVal && !nameInputVal) {
+    console.error("You need a search input value!");
+    return;
+  }
+  if (ingredientInputVal) {
+    searchInput = ingredientInputVal;
+  } else if (nameInputVal) {
+    searchInput = nameInputVal;
+  }
 
-    if (!ingredientInputVal && !nameInputVal) {
-        console.error('You need a search input value!');
-        return;
-    }
-    if (ingredientInputVal) {
-        searchInput = ingredientInputVal;
-    }
-
-    else if (nameInputVal) {
-        searchInput = nameInputVal;
-    }
-    
-        console.log(searchInput);
-    // location.assign(ingredientSearchInput);
+  console.log(searchInput);
+  // location.assign(ingredientSearchInput);
 }
 
 function ingredientSearch() {
-    var apiCall = 'https://cors-anywhere.herokuapp.com/http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + searchInput;
-    console.log(apiCall);
-    fetch(apiCall)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data.drinks)
+  var apiCall =
+    "https://cors-anywhere.herokuapp.com/www.thecocktaildb.com/api/json/v2/" + apiKey + "/filter.php?i=" + searchInput;
+  console.log(apiCall);
+  fetch(apiCall)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.drinks);
 
-            // for (let index = 0; index < data.drinks.length; index++) {
-            //     // const drinks = data.drinks[index];
-            //     }
+      // for (let index = 0; index < data.drinks.length; index++) {
+      //     // const drinks = data.drinks[index];
+      //     }
 
-          $('#recipe-display').text("Ingredient search result: " + data.drinks[0]["strDrink"])
-
-        })
-
+      $("#recipe-display").text("Ingredient search result: " + data.drinks[0]["strDrink"]);
+    });
 }
 
 // search by name function
 function nameSearch() {
-    var apiCall = 'https://cors-anywhere.herokuapp.com/http://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + searchInput;
-    console.log(apiCall);
-    fetch(apiCall)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data.drinks)
+  var apiCall =
+    "https://cors-anywhere.herokuapp.com/http://www.thecocktaildb.com/api/json/v2/" +
+    apiKey +
+    "/search.php?s=" +
+    searchInput;
+  console.log(apiCall);
+  fetch(apiCall)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.drinks);
 
-            // for (let index = 0; index < data.drinks.length; index++) {
-            //     // const drinks = data.drinks[index];
-            //     }
+      // for (let index = 0; index < data.drinks.length; index++) {
+      //     // const drinks = data.drinks[index];
+      //     }
 
-          $('#recipe-display').text("Cocktail search result: " + data.drinks[0]["strDrink"])
-
-        })
-
+      $("#recipe-display").text("Cocktail search result: " + data.drinks[0]["strDrink"]);
+    });
 }
-$('.ingredient-search').on('click', handleSearchFormSubmit);
-$('#ingredient-search').on('click', ingredientSearch);
+$(".ingredient-search").on("click", handleSearchFormSubmit);
+$("#ingredient-search").on("click", ingredientSearch);
 
-$('.name-search').on('click', handleSearchFormSubmit);
-$('#name-search').on('click', nameSearch);
-
+$(".name-search").on("click", handleSearchFormSubmit);
+$("#name-search").on("click", nameSearch);
