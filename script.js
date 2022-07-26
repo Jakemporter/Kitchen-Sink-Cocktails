1b824c19bb17cc91c89e85b53a263dbcbceac8e2
madeleine <mconanan@gmail.com>
2022-07-25 21:31:45 -0700

    finished name search

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 // search by ingredient fetch
 
-// to do: event prevent default, .push? to create a list of the drinks in display not just the last one
+// to do: .push? to create a list of the drinks in display not just the last one
 
-var ingredientSearchInput;
+var searchInput;
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
 
 
-    var searchInputVal = $('#ingredient-input').val()
-    console.log(searchInputVal);
-    if (!searchInputVal) {
+    var ingredientInputVal = $('#ingredient-input').val()
+    var nameInputVal = $('#cocktail-search').val()
+    
+
+    if (!ingredientInputVal && !nameInputVal) {
         console.error('You need a search input value!');
         return;
     }
+    if (ingredientInputVal) {
+        searchInput = ingredientInputVal;
+    }
 
-    ingredientSearchInput = searchInputVal
-
+    else if (nameInputVal) {
+        searchInput = nameInputVal;
+    }
+    
+        console.log(searchInput);
     // location.assign(ingredientSearchInput);
 }
 
 function ingredientSearch() {
-    var apiCall = 'https://cors-anywhere.herokuapp.com/http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredientSearchInput;
+    var apiCall = 'https://cors-anywhere.herokuapp.com/http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + searchInput;
     console.log(apiCall);
     fetch(apiCall)
         .then(function (response) {
@@ -34,11 +42,35 @@ function ingredientSearch() {
             //     // const drinks = data.drinks[index];
             //     }
 
-            $('#recipe-display').text(data.drinks[0]["strDrink"])
+          $('#recipe-display').text("Ingredient search result: " + data.drinks[0]["strDrink"])
 
         })
 
 }
 
+// search by name function
+function nameSearch() {
+    var apiCall = 'https://cors-anywhere.herokuapp.com/http://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + searchInput;
+    console.log(apiCall);
+    fetch(apiCall)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.drinks)
+
+            // for (let index = 0; index < data.drinks.length; index++) {
+            //     // const drinks = data.drinks[index];
+            //     }
+
+          $('#recipe-display').text("Cocktail search result: " + data.drinks[0]["strDrink"])
+
+        })
+
+}
 $('.ingredient-search').on('click', handleSearchFormSubmit);
 $('#ingredient-search').on('click', ingredientSearch);
+
+$('.name-search').on('click', handleSearchFormSubmit);
+$('#name-search').on('click', nameSearch);
+
