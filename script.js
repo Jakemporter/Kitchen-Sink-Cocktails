d831388887d76aa9571f677a03357f4313b4420e
madeleine <mconanan@gmail.com>
2022-07-26 20:44:00 -0700

    javascript updated; ingredient & cocktail name searches working, no error messages

--- script.js
+++ script.js
@@ -1,88 +1,94 @@
 // search by ingredient fetch
-//todo clear results before displaying again because I'm getting an error
-
-// to do: .push? to create a list of the drinks in display not just the last one
 var apiKey = keys.API_KEY;
 
 var searchInput;
 
 function handleSearchFormSubmit(event) {
-  event.preventDefault();
+    event.preventDefault();
+    $('#recipe-display').text('')
 
-  var ingredientInputVal = $("#ingredient-input").val();
-  var nameInputVal = $("#cocktail-search").val();
+    var ingredientInputVal = $("#ingredient-input").val();
+    var nameInputVal = $("#cocktail-search").val();
 
-  if (!ingredientInputVal && !nameInputVal) {
-    console.error("You need a search input value!");
-    return;
-  }
-  if (ingredientInputVal) {
-    searchInput = ingredientInputVal;
-  } else if (nameInputVal) {
-    searchInput = nameInputVal;
-  }
+    if (!ingredientInputVal && !nameInputVal) {
+        console.error("You need a search input value!");
+        return;
+    }
+    if (ingredientInputVal) {
+        searchInput = ingredientInputVal;
+    } else if (nameInputVal) {
+        searchInput = nameInputVal;
+    }
 
-  console.log(searchInput);
-  // location.assign(ingredientSearchInput);
+    console.log(searchInput);
+
+
+    $('#ingredient-input').val('')
+    $('#cocktail-search').val('')
 }
 
 function ingredientSearch() {
-  var apiCall =
-    "https://cors-anywhere.herokuapp.com/www.thecocktaildb.com/api/json/v2/" + apiKey + "/filter.php?i=" + searchInput;
-  console.log(apiCall);
-  fetch(apiCall)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data.drinks);
+    var apiCall =
+        "https://cors-anywhere.herokuapp.com/www.thecocktaildb.com/api/json/v2/" + apiKey + "/filter.php?i=" + searchInput;
+    console.log(apiCall);
+    fetch(apiCall)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.drinks);
 
-      // for (let index = 0; index < data.drinks.length; index++) {
-      //     // const drinks = data.drinks[index];
-      //     }
+            // for loop to create list of results
+            for (let i = 0; i < data.drinks.length; i++) {
+                if (data.drinks != "None Found") {
+                    $('#recipe-display').append(data.drinks[i]['strDrink'] + ' ' + data.drinks[i]['strDrinkThumb']);
+                }
 
-      $("#recipe-display").text(
-        "Ingredient search result: " +
-          data.drinks[0]["strDrink"] +
-          " " +
-          data.drinks[0]["strDrinkThumb"] +
-          " " +
-          data.drinks[0]["strInstructions"]
-      );
-    });
-}
+                else {
+                    $('#recipe-display').text("Ingredient not found. Please search a different ingredient.");
+                }
+
+            }
+
+        });
+
+};
+
+
+
 
 // search by name function
 function nameSearch() {
-  var apiCall =
-    "https://cors-anywhere.herokuapp.com/http://www.thecocktaildb.com/api/json/v2/" +
-    apiKey +
-    "/search.php?s=" +
-    searchInput;
-  console.log(apiCall);
-  fetch(apiCall)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data.drinks);
+    var apiCall =
+        "https://cors-anywhere.herokuapp.com/http://www.thecocktaildb.com/api/json/v2/" +
+        apiKey +
+        "/search.php?s=" +
+        searchInput;
+    console.log(apiCall);
+    fetch(apiCall)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.drinks);
 
-      // for (let index = 0; index < data.drinks.length; index++) {
-      //     // const drinks = data.drinks[index];
-      //     }
+            if (data.drinks != null) {
+                for (let i = 0; i < data.drinks.length; i++) {
 
-      $("#recipe-display").text(
-        "Ingredient search result: " +
-          data.drinks[0]["strDrink"] +
-          " " +
-          data.drinks[0]["strDrinkThumb"] +
-          " " +
-          data.drinks[0]["strInstructions"]
-      );
+                    $('#recipe-display').append(data.drinks[i]['strDrink'] + ' ' + data.drinks[i]['strDrinkThumb'] + ' ' + data.drinks[i]["strInstructions"]);
+                }
+            }
+
+            else {
+                $('#recipe-display').text("Drink not found. Please search a different drink.");
+            }
     });
 }
+
 $(".ingredient-search").on("click", handleSearchFormSubmit);
 $("#ingredient-search").on("click", ingredientSearch);
 
 $(".name-search").on("click", handleSearchFormSubmit);
 $("#name-search").on("click", nameSearch);
+
+
