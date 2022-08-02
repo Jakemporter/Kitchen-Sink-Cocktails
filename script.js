5b00974cd62cd811655a5d38457f1e3446675980
Jake Porter <Jake.m.porter@gmail.com>
2022-08-01 22:36:55 -0500

    added quote api and polished UI

--- script.js
+++ script.js
@@ -5,129 +5,130 @@ var apiKey = keys.API_KEY;
 
 var searchInput;
 
+function quoteOfTheDay() {
+  var apiCall = "https://favqs.com/api/qotd";
+  console.log(apiCall);
+  fetch(apiCall)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      $(".quote").append(data.quote.body + " Author: " + data.quote.author);
+      console.log(data);
+    });
+}
+
 function handleSearchFormSubmit(event) {
-    event.preventDefault();
-    $('#recipe-display').text('')
+  event.preventDefault();
+  $("#recipe-display").text("");
 
-    var ingredientInputVal = $("#ingredient-input").val();
-    var nameInputVal = $("#cocktail-search").val();
+  var ingredientInputVal = $("#ingredient-input").val();
+  var nameInputVal = $("#cocktail-search").val();
 
-    if (!ingredientInputVal && !nameInputVal) {
-        console.error("You need a search input value!");
-        return;
-    }
-    if (ingredientInputVal) {
-        searchInput = ingredientInputVal;
-    } else if (nameInputVal) {
-        searchInput = nameInputVal;
-    }
+  if (!ingredientInputVal && !nameInputVal) {
+    console.error("You need a search input value!");
+    return;
+  }
+  if (ingredientInputVal) {
+    searchInput = ingredientInputVal;
+  } else if (nameInputVal) {
+    searchInput = nameInputVal;
+  }
 
-    console.log(searchInput);
+  console.log(searchInput);
 
-
-    $('#ingredient-input').val('')
-    $('#cocktail-search').val('')
+  $("#ingredient-input").val("");
+  $("#cocktail-search").val("");
 }
 
 function ingredientSearch() {
-    var apiCall =
-        "https://cors-anywhere.herokuapp.com/www.thecocktaildb.com/api/json/v2/" + apiKey + "/filter.php?i=" + searchInput;
-    console.log(apiCall);
-    fetch(apiCall)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data.drinks);
+  var apiCall =
+    "https://cors-anywhere.herokuapp.com/www.thecocktaildb.com/api/json/v2/" + apiKey + "/filter.php?i=" + searchInput;
+  console.log(apiCall);
+  fetch(apiCall)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.drinks);
 
-            // for loop to create list of results
-            for (let i = 0; i < data.drinks.length; i++) {
-                if (data.drinks != "None Found") {
-                    $('#recipe-display').append(`
-                    <div class="columns">
-                    <div class="column is-half is-offset-one-quarter">
-                    <div class="card">
-                    <div class="card-image">
-                      <figure class="image is-128x128">
-                        <img src="${data.drinks[i]["strDrinkThumb"]}" alt="Cocktail Thumbnail">
-                      </figure>
-                    </div>
-                    <div class="card-content">
-                      <div class="media">
-                        <div class="media-content">
-                          <p class="title is-4">${data.drinks[i]["strDrink"]}</p>
-                          
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  </div>
-                  </div>`
-                  );
-                }
-
-                else {
-                    $('#recipe-display').text("Ingredient not found. Please search a different ingredient.");
-                }
-
-            }
-
-        });
-
-};
-
-
-
+      // for loop to create list of results
+      for (let i = 0; i < data.drinks.length; i++) {
+        if (data.drinks != "None Found") {
+          $("#recipe-display").append(`
+        <div class="columns">
+        <div class="column is-half is-offset-one-quarter">
+        <div class="card">
+        <div class="card-image">
+        <figure class="image is-128x128">
+        <img src="${data.drinks[i]["strDrinkThumb"]}" alt="Cocktail Thumbnail">
+        </figure>
+        </div>
+        <div class="card-content">
+        <div class="media">
+        <div class="media-content">
+        <p class="title is-4">${data.drinks[i]["strDrink"]}</p>
+        
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>`);
+        } else {
+          $("#recipe-display").addClass("bigger");
+          $("#recipe-display").text("Ingredient not found. Please search a different ingredient.");
+        }
+      }
+    });
+}
 
 // search by name function
 function nameSearch() {
-    var apiCall =
-        "https://cors-anywhere.herokuapp.com/http://www.thecocktaildb.com/api/json/v2/" +
-        apiKey +
-        "/search.php?s=" +
-        searchInput;
-    console.log(apiCall);
-    fetch(apiCall)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data.drinks);
+  var apiCall =
+    "https://cors-anywhere.herokuapp.com/http://www.thecocktaildb.com/api/json/v2/" +
+    apiKey +
+    "/search.php?s=" +
+    searchInput;
+  console.log(apiCall);
+  fetch(apiCall)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.drinks);
 
-            if (data.drinks != null) {
-                for (let i = 0; i < data.drinks.length; i++) {
-
-                    $('#recipe-display').append(`
-                    <div class="columns">
-                    <div class="column is-half is-offset-one-quarter">
-                    <div class="card">
-                    <div class="card-image">
-                      <figure class="image is-128x128">
-                        <img src="${data.drinks[i]["strDrinkThumb"]}" alt="Cocktail Thumbnail">
-                      </figure>
-                    </div>
-                    <div class="card-content">
-                      <div class="media">
-                        <div class="media-content">
-                          <p class="title is-4">${data.drinks[i]["strDrink"]}</p>
-                          
-                        </div>
-                      </div>
-                  
-                      <div class="content">
-                        ${data.drinks[i]["strInstructions"]}
-                      </div>
-                    </div>
-                  </div>
-                  </div>
-                  </div>`
-                  );
-                }
-            }
-
-            else {
-                $('#recipe-display').text("Drink not found. Please search a different drink.");
-            }
+      if (data.drinks != null) {
+        for (let i = 0; i < data.drinks.length; i++) {
+          $("#recipe-display").append(`
+        <div class="columns">
+        <div class="column is-half is-offset-one-quarter">
+        <div class="card">
+        <div class="card-image">
+        <figure class="image is-128x128">
+        <img src="${data.drinks[i]["strDrinkThumb"]}" alt="Cocktail Thumbnail">
+        </figure>
+        </div>
+        <div class="card-content">
+        <div class="media">
+        <div class="media-content">
+        <p class="title is-4">${data.drinks[i]["strDrink"]}</p>
+        
+        </div>
+        </div>
+        
+        <div class="content">
+        ${data.drinks[i]["strInstructions"]}
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>`);
+        }
+      } else {
+        $("#recipe-display").addClass("bigger");
+        $("#recipe-display").text("Drink not found. Please search a different drink.");
+      }
     });
 }
 
@@ -137,4 +138,4 @@ $("#ingredient-search").on("click", ingredientSearch);
 $(".name-search").on("click", handleSearchFormSubmit);
 $("#name-search").on("click", nameSearch);
 
-
+quoteOfTheDay();
